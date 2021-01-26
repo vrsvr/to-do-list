7071f24542bf0886fca868094e6483725d2062f4
vrsvr <rifatvarisov@gmail.com>
2021-01-26 15:12:56 +0500

    init, navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="blue darken-1">
+    <div class="nav-wrapper">
+      <router-link to="/" class="brand-logo">Tasks</router-link>
+      <ul class="right hide-on-med-and-down">
+        <router-link tag="li" to="/" exact active-class="active"><a href="#">Create</a></router-link>
+        <router-link tag="li" to="/list" active-class="active"><a href="#">List</a></router-link>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+  nav {
+    padding: 0 2rem;
+  }
+</style>
